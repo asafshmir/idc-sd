6395c30cc248b47575603bfb5eca0b59835ed732
asafshmir <asafshmir@gmail.com>
2016-01-09 17:23:16 +0200

    KeysManager - merged

--- davdroid/app/src/main/java/at/bitfire/davdroid/crypto/KeyManager.java
+++ davdroid/app/src/main/java/at/bitfire/davdroid/crypto/KeyManager.java
@@ -7,6 +7,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.security.KeyPair;
+import java.security.PublicKey;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,8 +34,11 @@ public class KeyManager {
     private final static String ENC_SK_ATTR = "enc-sk";
     private final static String SIGNATURE_ATTR = "signature";
 
-    // userID to KeyRecord
+    // A map from userID to KeyRecord
+    protected String userID;
     protected Map<String, KeyRecord> keyBank;
+
+    // Asymmetric key-pair
     protected KeyPair asymKeyPair;
 
     public KeyManager(String data)  {
@@ -51,25 +56,31 @@ public class KeyManager {
 
     public String initKeyBank(String userID, String keyBankData) {
 
-        // TODO remember which user is 'me'
+        this.userID = userID;
+        byte[] pbkey = asymKeyPair.getPublic().getEncoded();
+        byte[] signature = CryptoUtils.calculateSignature(pbkey, getSecret(this.userID));
 
         // KeyBank is empty, create the first record
         if (keyBankData == null) {
 
             byte[] sk = CryptoUtils.generateRandomSymmetricKey();
             byte[] encSK = CryptoUtils.encryptSymmetricKey(sk, asymKeyPair.getPublic());
-
-            byte[] signedData = new byte[sk.length + encSK.length];
-            // TODO figure out which key to use instead of 'secret'
-            byte[] signature = CryptoUtils.calculateSignature(signedData, "secret".getBytes());
-
-            keyBank.put(userID, new KeyRecord(asymKeyPair.getPublic().getEncoded(), encSK, signature));
+            keyBank.put(userID, new KeyRecord(pbkey, encSK, signature));
 
         } else {
             keyBank = stringToKeyBank(keyBankData);
+            // TODO handle corrupted keyBankData?
+
+            // Add a KeyRecord if userID is not in yet in the KeyBank
+            // The encSK is empty because this user is not validated yet
+            // TODO if you are not the owner, add a request record
+            if (!keyBank.containsKey(userID)) {
+                keyBank.put(userID, new KeyRecord(pbkey, null, signature));
+            }
         }
 
-        // TODO if you are the owner, validate all other records and add their encSK
+        // TODO only if you are the owner, validate all other records and add their encSK
+        validateAllUsers();
 
         // Return the new key-bank
         return keyBankToString();
@@ -78,20 +89,82 @@ public class KeyManager {
     public byte[] getSK(String userID) {
 
         KeyRecord keyRecord = keyBank.get(userID);
-        if (keyRecord == null) {
+        // No such user
+        if (keyRecord == null)
             return null;
-        }
 
         byte[] encSK = keyRecord.encSK;
-        return CryptoUtils.decryptSymmetricKey(keyRecord.encSK, asymKeyPair.getPrivate());
+        // User is not validated yet
+        if (encSK == null)
+            return null;
+
+        return CryptoUtils.decryptSymmetricKey(encSK, asymKeyPair.getPrivate());
     }
 
     public void addUserRequest(String userID, byte[] pbkey) {
 
     }
 
-    public void validateAllUsers() {
+    private void validateAllUsers() {
 
+        // TODO make sure i can validate users - i.e owner
+        byte[] realSK = getSK(this.userID);
+        // My user doesn't have a valid SK so it can't validate others
+        if (realSK == null)
+            return;
+
+        // Iterate the users validate them
+        for (String userID : keyBank.keySet()) {
+
+            // No need to validate my user
+            if (this.userID.equals(userID))
+                continue;
+
+            KeyRecord keyRecord = keyBank.get(userID);
+            // User has an SK, so we don't need to validate it
+            if (keyRecord.encSK != null)
+                continue;
+
+            // Validate the user's KeyRecord
+            boolean valid = validateSignature(userID, keyRecord);
+            final byte[] pbKeyBytes = keyRecord.pbkey;
+            // If valid, add an encrypted version of SK to the user KeyRecord
+            if (valid) {
+                PublicKey userPbKey = new PublicKey() {
+                    @Override
+                    public String getAlgorithm() {return null;}
+                    @Override
+                    public String getFormat() {return null;}
+                    @Override
+                    public byte[] getEncoded() {return pbKeyBytes;}
+                };
+                keyRecord.encSK = CryptoUtils.encryptSymmetricKey(realSK, userPbKey);
+            }
+        }
+    }
+
+    private boolean validateSignature(String userID, KeyRecord keyRecord) {
+        if (keyRecord == null) return false;
+        return validateSignature(keyRecord.signature, keyRecord.pbkey, userID);
+    }
+
+    private boolean validateSignature(byte[] signature, byte[] pbkey, String userID) {
+
+        // TODO use signature without a key, and sign pbkey+secret together
+
+        // Get the user's secret
+        byte[] secret = getSecret(userID);
+
+        // Apply the signature process on the given public-key
+        byte[] realSignature = CryptoUtils.calculateSignature(pbkey, secret);
+
+        // Compare the given signature with the real one
+        return Arrays.equals(realSignature, signature);
+    }
+
+    // TODO figure out which key to use instead of 'secret'
+    private byte[] getSecret(String userID) {
+        return "secret".getBytes();
     }
 
     private Map<String, KeyRecord> stringToKeyBank(String data) {
@@ -103,7 +176,7 @@ public class KeyManager {
             JSONArray keybank = rootObj.optJSONArray(KEYBANK_TAG);
 
             // Iterate the JSONArray and init users
-            for (int i=0; i < keybank.length(); i++){
+            for (int i = 0; i < keybank.length(); i++) {
                 JSONObject userObj = keybank.getJSONObject(i);
 
                 // Read user data
