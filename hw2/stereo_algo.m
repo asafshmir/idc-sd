1b6a965f80a705b44f590f31ea954f9d2d8b5a26
Asaf Shamir <Asaf Shamir>
2016-12-20 23:35:39 +0200

    use cosine distance with it's negative value

--- hw2/stereo_algo.m
+++ hw2/stereo_algo.m
@@ -2,14 +2,13 @@ function stereo_algo
     im1=readImage('view5.tif');
     im2=readImage('view1.tif');
     
-    patch_width = 3;
-    patch_height = 3;
+    patch_width = 1;
+    patch_height = 1;
 
     disparityRange = [10 140];
 %     disparityMap = disparity(im1, im2, 'Method', 'SemiGlobal', 'BlockSize', 5, 'DisparityRange', disparityRange);
     disparityMap = ComputeDisparityMap(im1, im2, disparityRange, 1, 1);
 
-   
     figure
     imshow(disparityMap, disparityRange);
     colorbar
@@ -23,23 +22,23 @@ function D = ComputeDisparityMap(im1, im2, disparityRange, patch_height, patch_w
     D = zeros(size(im1,1), size(im1,2));
     for h = patch_height+1:(size(im1, 1)-patch_height) 
         for l = patch_width+1:(size(im1, 2)-patch_width)
-           min_dist = 1;
-           min_disparity = im_width;
-           
-           from_x = max(patch_width+1, l + disparityRange(1));
-           to_x = min(im_width-patch_width, l + disparityRange(2));
-           for r = from_x:(to_x)
-               p1 = [h, l];
-               p2 = [h, r];
-               dist = ComputeRectDistance(im1, im2, p1, p2, patch_height, patch_width);       
-               if (dist < min_dist)
-                   min_dist = dist;
-                   min_disparity = r - l;
-               end
-           end
-           D(h, l) = min_disparity;
-       end
-   end
+            min_dist = 1;
+            min_disparity = im_width;
+
+            from_x = max(patch_width+1, l + disparityRange(1));
+            to_x = min(im_width-patch_width, l + disparityRange(2));
+            for r = from_x:(to_x)
+                p1 = [h, l];
+                p2 = [h, r];
+                dist = ComputeRectDistance(im1, im2, p1, p2, patch_height, patch_width);       
+                if (dist < min_dist)
+                    min_dist = dist;
+                    min_disparity = r - l;
+                end
+            end
+            D(h, l) = min_disparity;
+        end
+    end
 end
 
 function dist = ComputeRectDistance(im1, im2, p1, p2, patch_height, patch_width)
@@ -59,7 +58,12 @@ function dist = ComputeRectDistance(im1, im2, p1, p2, patch_height, patch_width)
     vec1 = double(reshape(im1(x1start:x1end, y1start:y1end), 1, n_pixels));
     vec2 = double(reshape(im2(x2start:x2end, y2start:y2end), 1, n_pixels));
 
-    dist = cosine_distance(vec1, vec2);
+    dist = cosine_distance(vec1, vec2); 
+    
+    % Use negative values so smaller value will represent smaller distance
+    % The mininmal value is 1, which means that the vectors have the same
+    % direction
+    dist = -dist;
 end
 
 function result = cosine_distance(vec1,vec2)
