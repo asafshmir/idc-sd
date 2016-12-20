17665565b8a66852be75002a1b329c3435dc82ed
Asaf Shamir <Asaf Shamir>
2016-12-20 21:49:48 +0200

    fix ComputeDisparityMap - save disparity and not distance

--- hw2/stereo_algo.m
+++ hw2/stereo_algo.m
@@ -2,13 +2,12 @@ function stereo_algo
     im1=readImage('view5.tif');
     im2=readImage('view1.tif');
     
-    patch_width = 1;
-    patch_height = 1;
-%     disparityRange = [-25 25];
-%     disparityMap = ComputeDisparityMap(im1, im2, disparityRange, patch_width, patch_height);
-    disparityRange = [0 32];
-    disparityMap = disparity(im1, im2, 'BlockSize', 5, 'DisparityRange', disparityRange);
-    disparityMap2 = ComputeDisparityMap(im1, im2, disparityRange, 1, 1);
+    patch_width = 3;
+    patch_height = 3;
+
+    disparityRange = [10 140];
+%     disparityMap = disparity(im1, im2, 'Method', 'SemiGlobal', 'BlockSize', 5, 'DisparityRange', disparityRange);
+    disparityMap = ComputeDisparityMap(im1, im2, disparityRange, 1, 1);
 
    
     figure
@@ -17,44 +16,45 @@ function stereo_algo
     hold on
 end
 
-function D = ComputeDisparityMap(im1, im2, disparityRange, patch_width, patch_height)
+function D = ComputeDisparityMap(im1, im2, disparityRange, patch_height, patch_width)
 
-    im_size = size(im1,2);
+    im_width = size(im1,2);
 
     D = zeros(size(im1,1), size(im1,2));
-    for l = patch_width+1:(size(im1, 1)-patch_width) 
-        for m = patch_height+1:(size(im1, 2)-patch_height)
-           min_disparity = im_size;
+    for h = patch_height+1:(size(im1, 1)-patch_height) 
+        for l = patch_width+1:(size(im1, 2)-patch_width)
+           min_dist = 1;
+           min_disparity = im_width;
            
-           %for j = patch_width+1:(im_size-patch_size)
-           from_x = max(patch_width+1, m + disparityRange(1));
-           to_x = min(im_size-patch_width, m + disparityRange(2));
-           for j = from_x:(to_x)
-               p1 = [l, m];
-               p2 = [l, j];
-               disparity = ComputeRectDistance(im1, im2, p1, p2, patch_width, patch_height);       
-               if (disparity < min_disparity)
-                   min_disparity = disparity;
+           from_x = max(patch_width+1, l + disparityRange(1));
+           to_x = min(im_width-patch_width, l + disparityRange(2));
+           for r = from_x:(to_x)
+               p1 = [h, l];
+               p2 = [h, r];
+               dist = ComputeRectDistance(im1, im2, p1, p2, patch_height, patch_width);       
+               if (dist < min_dist)
+                   min_dist = dist;
+                   min_disparity = r - l;
                end
            end
-           D(l, m) = min_disparity;
+           D(h, l) = min_disparity;
        end
    end
 end
 
-function dist = ComputeRectDistance(im1, im2, p1, p2, patch_width, patch_height)
+function dist = ComputeRectDistance(im1, im2, p1, p2, patch_height, patch_width)
 
-    x1start = (p1(1)-patch_width);
-    x1end = (p1(1)+patch_width);
-    y1start = (p1(2)-patch_height);
-    y1end = (p1(2)+patch_height);
+    x1start = (p1(1)-patch_height);
+    x1end = (p1(1)+patch_height);
+    y1start = (p1(2)-patch_width);
+    y1end = (p1(2)+patch_width);
     
-    x2start = (p2(1)-patch_width);
-    x2end = (p2(1)+patch_width);
-    y2start = (p2(2)-patch_height);
-    y2end = (p2(2)+patch_height);
+    x2start = (p2(1)-patch_height);
+    x2end = (p2(1)+patch_height);
+    y2start = (p2(2)-patch_width);
+    y2end = (p2(2)+patch_width);
     
-    n_pixels = (patch_width*2 + 1)*(patch_height*2 + 1);
+    n_pixels = (patch_height*2 + 1)*(patch_width*2 + 1);
     
     vec1 = double(reshape(im1(x1start:x1end, y1start:y1end), 1, n_pixels));
     vec2 = double(reshape(im2(x2start:x2end, y2start:y2end), 1, n_pixels));
