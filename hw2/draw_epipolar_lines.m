d0dfe87279c9ed79d6b6ec6f8c4acd5d71b934d3
Asaf Shamir <Asaf Shamir>
2016-12-25 07:19:48 +0200

    cosmetic changes

--- hw2/draw_epipolar_lines.m
+++ hw2/draw_epipolar_lines.m
@@ -2,12 +2,14 @@ function draw_epipolar_lines = draw_epipolar_lines(im1,im2,F,p,f1,f2)
     P = [p(1) p(2) 1]';
     
     figure(f1);
-    plot(p(1),p(2),'*r');
+    plot(p(1),p(2),'*y');
     
     l = F*P;
     figure(f2);
-    plot(l(1),l(2),'*r');
+    plot(l(1),l(2),'*y');
     
     points = lineToBorderPoints(l',size(im1)); 
     line(points(:,[1,3])',points(:,[2,4])');
+    
+    hold on
 end
