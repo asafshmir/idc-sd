fef24bfba7bdc29012cd82583978132cd212c53b
Jonatan Russo <russo.jonatan@gmail.com>
2017-01-09 00:03:38 +0200

    Checkpoint

--- hw3/OF.m
+++ hw3/OF.m
@@ -5,10 +5,20 @@ function [u,v] = OF(F1, F2, Smooth, Region)
 
 Gx = xx .* exp(-(xx .^ 2 + yy .^ 2) / (2 * Smooth ^ 2));
 Gy = yy .* exp(-(xx .^ 2 + yy .^ 2) / (2 * Smooth ^ 2));
+Gt = yy .* exp(-(xx .^ 2 + yy .^ 2) / (2 * Smooth ^ 2));
 
-Ix = conv2(im, Gx, 'same');
-Iy = conv2(im, Gy, 'same');
+Ix = conv2(F1, Gx, 'same');
+Iy = conv2(F1, Gy, 'same');
+It = F2-F1;
 
+A = size(Region, Region);
+b = It;
+for i = 1:Region
+    for j = 1:Region
+        A(i,j) = [Ix(i,j)/It(i,j); Iy(i,j)/It(i,j)]; 
+    end
+end
+Ainv = pinv(A);
 
 end
 
